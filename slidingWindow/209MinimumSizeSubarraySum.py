730356b77d3dd09ff3417702cee75a9d8f9cc2ae
danbazo <63355975+danbazo@users.noreply.github.com>
2025-04-08 14:20:04 -0300

    Update 209MinimumSizeSubarraySum.py

--- slidingWindow/209MinimumSizeSubarraySum.py
+++ slidingWindow/209MinimumSizeSubarraySum.py
@@ -1,18 +1,18 @@
 def minSubArrayLen(target, nums):
         minLength=0
-        end=False
+        
         front=0
         currentSum=nums[0]
         totalLength=len(nums)
         for i in range(totalLength):
             while currentSum<target:
                 if front==totalLength-1: 
-                    end=True
-                    break
+                    return minLength
+                    
                 front+=1
                 currentSum+=nums[front]
                 
-            if end: break
+            
             if minLength:
                 minLength=min(minLength,front-i+1)
             else:
