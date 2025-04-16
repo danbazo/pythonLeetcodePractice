e6b59ef0846b8c897571e11d08c80622b61100fe
danbazo <63355975+danbazo@users.noreply.github.com>
2025-04-16 10:44:09 -0300

    Update 001TwoSum.py
    Changed the code with a linear approach using a Dictionary (Hashmap) to improve speed

--- hashmap/001TwoSum.py
+++ hashmap/001TwoSum.py
@@ -1,12 +1,18 @@
 def twoSum(nums, target):
-  indexes=[i for i in range(len(nums))]
+  """
+  returns a list with the indexes of the two integers in nums that sum up the target value.
 
-  sortedNums, sortedIndexes = zip(*sorted(zip(nums,indexes)))
-  start=0
-  end=len(nums)-1
-  while True:
-    if sortedNums[start]+sortedNums[end]==target:
-        return [sortedIndexes[start],sortedIndexes[end]]
-    elif sortedNums[start]+sortedNums[end]>target:
-        end-=1
-    else: start+=1
+  Keyword arguments:
+  nums a list of integers
+  target an integer
+  """
+  #Initialize the dictionary where the values and indexes of nums will be stored
+  numsDict={}
+
+  #loop though the list nums, storing the values and indexes and comparing if we find the sum of the target value
+  for i in range(len(nums)):
+    complement=target-nums[i]
+    if complement in numsDict:
+      return [i,numsDict[complement]]
+    else:
+      numsDict[nums[i]]=i
