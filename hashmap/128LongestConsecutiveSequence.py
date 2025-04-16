76793570c22a49325408e66150fe160db1299943
danbazo <63355975+danbazo@users.noreply.github.com>
2025-04-16 14:57:35 -0300

    Update 128LongestConsecutiveSequence.py
    third attempt, most optimal solution so far, both in terms of speed and memory

--- hashmap/128LongestConsecutiveSequence.py
+++ hashmap/128LongestConsecutiveSequence.py
@@ -1,22 +1,30 @@
 def longestConsecutive(nums):
-    numsDict={}
+    """
+    returns a the length of the longest consecutive sequence of numbers in nums.
+    
+    Keyword arguments:
+    nums a list of integers, unordered
+    
+    """
+    
+    #turn nums into a set for a faster lookup
+    numsSet=set(nums)
+    #Initialize the answer value as 0
     longest=0
-    for element in nums:
-        if element in numsDict:
-        continue
-        numsDict[element]=[element,element]
-        upper=element
-        lower=element
-        if element+1 in numsDict:
-            upper=numsDict[element+1][1]
+    #Iterate over the whole set
+    for element in numsSet:
+        #if the previous number is in the set, its not the start of the sequence, so we continue     
+        if element-1 in numsSet:
+            continue
+        #Initilize the current number value and the currentLength    
+        currentNum=element
+        currentLength=1
 
-        if element-1 in numsDict:
-            lower=numsDict[element-1][0]
-        longest=max(upper-lower+1,longest)
-        
-        numsDict[lower]=[lower,upper]
-        numsDict[upper]=[lower,upper]
-    
-    
-    
+        #Check if next number is in set, and increase both length and number until next is not found
+        while currentNum+1 in numsSet:
+            currentNum+=1
+            currentLength+=1
+        #check which is longest between the current length and the longest yet    
+        longest=max(longest,currentLength)
+       
     return longest
