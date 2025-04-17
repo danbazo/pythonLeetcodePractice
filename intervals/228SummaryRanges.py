a6f0c3081036c423931e50882abb1d8c0d7e9660
danbazo <63355975+danbazo@users.noreply.github.com>
2025-04-17 12:14:55 -0300

    Update 228SummaryRanges.py
    Updated for cleaner code and added comments

--- intervals/228SummaryRanges.py
+++ intervals/228SummaryRanges.py
@@ -1,18 +1,30 @@
 def summaryRanges(nums):
+    """
+    returns a list of strings indicating the start and end of each range within nums.
+    
+    Keyword arguments:
+    nums a list of integers, unique and sorted
+  
+    """
+    #Initialize the list ranges, which will be the returned object
     ranges=[]
+    #if nums is empty, an empty list will be returned
     if not nums:
       return ranges
+    #Initialize the first value of the starting number of the ranges
     start=nums[0]
-    i=0
-    while i<len(nums)-1:
+    
+    #Loop once though the whole nums list, save the last number
+    for i in range(len(nums)-1):
+        #If the next number is not the current one plus one, finish the range and append its string into ranges
       if nums[i+1]!=nums[i]+1:
         if start==nums[i]:
           ranges.append(str(start))
         else:
-          ranges.append(str(start)+'->'+str(nums[i]))
+          ranges.append(f"{start}->{nums[i]}")
         start=nums[i+1]
 
-      i+=1
+    #Finish the last range
     if start==nums[-1]:
       ranges.append(str(start))
     else:
