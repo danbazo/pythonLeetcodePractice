4f6a2232e8f53ab4c411c7e5461a5b4d27a801a5
danbazo <63355975+danbazo@users.noreply.github.com>
2025-05-01 13:32:55 -0300

    Update evalRPN.py

--- stack/evalRPN.py
+++ stack/evalRPN.py
@@ -5,15 +5,15 @@ def evalRPN(tokens):
     Keyword arguments:
     tokens a list of strings that represents an arithmetic expression in a Reverse Polish Notation.
     """
-  #Initialize the stack list
-  stack=[]
-  #Create a dictionary opts to convert the string operators into functions
-  ops={'+':lambda x, y: x+y,
+   #Initialize the stack list
+   stack=[]
+   #Create a dictionary opts to convert the string operators into functions
+   ops={'+':lambda x, y: x+y,
        '/':lambda x, y: int(x/y), #Its requested that division operation truncates toward zero.
        '-':lambda x,y:x-y,
        '*':lambda x,y:x*y}
-  #Iterate over the list tokens
-  for element in tokens:
+   #Iterate over the list tokens
+   for element in tokens:
     #Any time an operator is found, said operation is applied to the last two items in the stack, and the result is appended at the end
     if element in ops:
       second=stack.pop()
@@ -24,9 +24,9 @@ def evalRPN(tokens):
     #If a number is found, its appended at the end of the stack as an integer
     else:
       stack.append(int(element))
-  #The last remaining element of the stack is returned
-  return stack[0]
-#Example test
-if __name__ == "__main__":
-  print('Evaluate Reverse Polish Notation test. tokens = ["2","1","+","3","*"]')
-  print('Returns: ', evalRPN(["2","1","+","3","*"]))
+   #The last remaining element of the stack is returned
+   return stack[0]
+   #Example test
+   if __name__ == "__main__":
+      print('Evaluate Reverse Polish Notation test. tokens = ["2","1","+","3","*"]')
+      print('Returns: ', evalRPN(["2","1","+","3","*"]))
