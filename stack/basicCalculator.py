42b87455e5c48b73b18c40f464cb90d4e7d40398
danbazo <63355975+danbazo@users.noreply.github.com>
2025-05-02 11:32:45 -0300

    Update basicCalculator.py
    thrid and last iteration of this code. Returned to an stack implementation, simplifying it

--- stack/basicCalculator.py
+++ stack/basicCalculator.py
@@ -1,34 +1,55 @@
 def calculate(s):
+        """
+        returns an integer value of the result of the calculations in the string s
+
+        Keyword arguments:
+        s a string of a valid expresion of a calculation, expressed in numbers, spaces, '+' and '-' signs and parentheses
+        """
+        #Create a dictionary opts to convert the string operators into functions
         ops={'+':lambda x,y:x+y,
         '-': lambda x,y:x-y}
-        i=0
-        def helper(s,i):
-            total=0
-            num=0
-            operation='+'
-            
-            while i<len(s):
-                if s[i].isdigit():
-                    num=num*10+int(s[i])
-                
+        
+        #Initialize the number variable, to add digits of numbers found
+        num=''
+        #Initialize the sign as positive, to keep track of the last operator found
+        sign='+'
+        #Initialize the stack, where the operations are being handled, and parenthesis are kept track of
+        stack=[0]
+        #Iterate over the string s
+        for character in s:
+                #If the character is a number, we add it to the current num string
+                if character.isdigit():
+                        num+=character
+                #If its an empty space, we skip the loop
+                elif character==' ':continue
+                #Else, it will be an operator or parentheses
                 else:
-                    if num:
-                        total=ops[operation](total,num)
-                        num=0
-                    if s[i]==' ':
-                        pass
-                    elif s[i]=='(':
-                        toSum,iIncreased=helper(s,i+1)
-                        total=ops[operation](total,toSum)
-                        i=iIncreased
-                    elif s[i]==')':
-                        
-                        return total,i
-                    else:
-                        operation=s[i]
-                        
-                i+=1
-            if num:
-                total=ops[operation](total,num)
-            return total,i
-        return helper(s,i)[0]
+                        #If we have a number num, we add it to the lastest value in the stack with the current sign and reset num
+                        if num:
+                            stack[-1]=ops[sign](stack[-1],int(num))
+                            num=''
+                        #If the character is an operator, we update the sign
+                        if character in ops:
+                            sign=character
+                        #Else, if its an opening bracket, we append the current sign to the stack and a cero, initilizing a new calculation inside a bracket, and reset the sign as '+'
+                        elif character=='(':
+                            
+                            stack.append(sign)
+                            stack.append(0)
+                            sign='+'
+                        #Else, the character will be a closing bracket, in which case the last number of the stack (the sum inside the brackets) will be added using the operator in the stack to the previous number
+                        #This handles nested brackets correctly
+                        else:
+                            
+                            y=stack.pop()
+                            operation=stack.pop()
+                            stack[-1]=ops[operation](stack[-1],y)
+        #In the end, if there are still a number left not added, its done so
+        if num: stack[-1]+=int(sign+num)
+        #Return the only left number in the stack
+        return stack[0]
+
+#Example test
+if __name__ == "__main__":
+   print('Basic Calculator test. s = "(1+(4+5+2)-3)+(6+8)"')
+   print('Returns: ', calculate("(1+(4+5+2)-3)+(6+8)"))
