851049eb8fc31d1285baaa9ad550c969b2716ca8
danbazo <63355975+danbazo@users.noreply.github.com>
2025-05-06 17:00:24 -0300

    Update trappingRainWater.py
    Updated code to a more simple two poiter approach

--- arrays/trappingRainWater.py
+++ arrays/trappingRainWater.py
@@ -1,53 +1,31 @@
 def trap(height):
-        #We initialize the variables we will use
+        """
+        returns an integer value of the sum of water trapped in the elevations given in height
+        
+        Keyword arguments:
+        height a list of integers representing a 2D elevation map
+        """
+        
+        #initialize the variables
         waterSum=0
-        maxHeight=[]
-        maxHeightIndex=[]
-        up=True
-        height.append(0)
-       
-        #We start our first loop, to determine the local maximums in the height array
-        for i in range(len(height)-1):
-            if up:
-                if height[i]>height[i+1]:
-                    maxHeight.append(height[i])
-                    maxHeightIndex.append(i)
-                    up=False
+        maxLeft=0
+        maxRight=0
+        left=0
+        right=len(height)-1
+        #Iterate over the height list from left and right using a 2 pointer approach
+        while left<right:
+            if height[left]<height[right]:
+                if height[left]>maxLeft:
+                    maxLeft=height[left]
+                else:
+                    waterSum+=maxLeft-height[left]
+                left+=1
             else:
-                if height[i]<height[i+1]:
-                    up=True
-       
-        #if no loca maxiumums were found, return 0, as there will be no "valleys" for the water to deposit
-        if not maxHeight:
-            return 0
-       
-        #We start our second loop, this time on the array we created for the Maximums, to eliminate the ones between greater ones, as the water will be over those
-        currentHighest=maxHeight[-1]
-        for j in reversed(range(len(maxHeight)-1)):
-            if maxHeight[j]<=currentHighest:
-                while maxHeight[j]>maxHeight[j+1]:
-                    del maxHeight[j+1]
-                    del maxHeightIndex[j+1]
-            else:
-                while currentHighest>maxHeight[j+1]:
-                    del maxHeight[j+1]
-                    del maxHeightIndex[j+1]
-                currentHighest=maxHeight[j]
-        #We initialize th variables for out last loop, to calculate the water deposited, using the maximums array we crated, and the index array as well
-        prevIndex=0
-        nextIndex=maxHeightIndex[0]
-        prevLevel=0
-        nextLevel=maxHeight[0]
-        for k in range(1,len(maxHeight)):
-            prevIndex=nextIndex
-            nextIndex=maxHeightIndex[k]
-            prevLevel=nextLevel
-            nextLevel=maxHeight[k]
-            level=min(prevLevel,nextLevel)
-            for h in range(prevIndex+1,nextIndex):
-                diff=level-height[h]
-                if diff>0:
-                    waterSum+=(diff)
+                if height[right]>maxRight:
+                    maxRight=height[right]
+                else:
+                    waterSum+=maxRight-height[right]
+                right-=1
         return waterSum
 #Example test
 if __name__ == "__main__":
